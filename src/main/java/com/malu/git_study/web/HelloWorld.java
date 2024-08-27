8a7a3e23b60178e9cb66539b214b5cf5b3ab5860
zyj <zyj@qq.com>
2024-08-27 17:36:59 +0800

    第二次上传

--- src/main/java/com/malu/git_study/web/HelloWorld.java
+++ src/main/java/com/malu/git_study/web/HelloWorld.java
@@ -2,6 +2,6 @@ package com.malu.git_study.web;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World11111");
     }
 }
